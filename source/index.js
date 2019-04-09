28d4f40efb09ac8a1d486b8933b25f67beb38b3a
Billgo <cocobill@vip.qq.com>
2019-04-09 14:50:59 +0800

    add eslint

--- source/index.js
+++ source/index.js
@@ -6,7 +6,7 @@ module.exports = function(context, opts = {}) {
     modules = 'auto',
     targets = {
       browsers: ['last 2 versions'],
-      node: 'current'
+      node: 'current',
     },
     env = {},
   } = opts;
@@ -25,25 +25,14 @@ module.exports = function(context, opts = {}) {
   const plugins = [
     require.resolve('babel-plugin-react-require'),
     require.resolve('@babel/plugin-syntax-dynamic-import'),
-    [
-      require.resolve('@babel/plugin-proposal-object-rest-spread'),
-      { loose: true, useBuiltIns: true },
-    ],
+    [require.resolve('@babel/plugin-proposal-object-rest-spread'), { loose: true, useBuiltIns: true }],
     require.resolve('@babel/plugin-proposal-optional-catch-binding'),
     require.resolve('@babel/plugin-proposal-async-generator-functions'),
-
     [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
-    [
-      require.resolve('@babel/plugin-proposal-class-properties'),
-      { loose: true },
-    ],
-
+    [require.resolve('@babel/plugin-proposal-class-properties'), { loose: true }],
     require.resolve('@babel/plugin-proposal-export-namespace-from'),
     require.resolve('@babel/plugin-proposal-export-default-from'),
-    [
-      require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
-      { loose },
-    ],
+    [require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'), { loose }],
     [require.resolve('@babel/plugin-proposal-optional-chaining'), { loose }],
     [
       require.resolve('@babel/plugin-proposal-pipeline-operator'),
@@ -53,22 +42,15 @@ module.exports = function(context, opts = {}) {
     ],
     require.resolve('@babel/plugin-proposal-do-expressions'),
     require.resolve('@babel/plugin-proposal-function-bind'),
-    require.resolve('@babel/plugin-external-helpers'),
     require.resolve('babel-plugin-add-module-exports'),
-    require.resolve('babel-plugin-macros'),
   ];
 
   if (nodeEnv !== 'test' && transformRuntime) {
-    plugins.push([
-      require.resolve('@babel/plugin-transform-runtime'),
-      transformRuntime,
-    ]);
+    plugins.push([require.resolve('@babel/plugin-transform-runtime'), transformRuntime]);
   }
 
   if (nodeEnv === 'production') {
-    plugins.push(
-      require.resolve('babel-plugin-transform-react-remove-prop-types'),
-    );
+    plugins.push(require.resolve('babel-plugin-transform-react-remove-prop-types'));
   }
 
   return {
@@ -89,4 +71,4 @@ module.exports = function(context, opts = {}) {
     ],
     plugins,
   };
-}
+};
